cd679077c6f6e6c9ed9b31d2c663d8d710f28332
niuge168 <92422086+niuge168@users.noreply.github.com>
2022-09-01 16:42:50 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     goerli: {
       url: process.env.REACT_APP_ALCHEMY_API_URL,
-      accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
+      accounts: [ process ]
     }
   },
   solidity: {
